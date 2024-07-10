566db21c600956e05de9316cd5257f976d3e64d7
fioreGH <rfiore@gmail.com>
2024-07-10 17:17:40 -0300

    implement and create class

--- src/main/java/com/eventostec/api/domain/event/EventRequestsDTO.java
+++ src/main/java/com/eventostec/api/domain/event/EventRequestsDTO.java
@@ -4,5 +4,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Date;
 
-public record EventRequestsDTO(String title, String Description, Date date, String city, String state, Boolean remote, String eventUrl, MultipartFile image) {
+public record EventRequestsDTO(String title, String Description, Long date, String city, String state,
+                               Boolean remote, String eventUrl, MultipartFile image) {
 }
